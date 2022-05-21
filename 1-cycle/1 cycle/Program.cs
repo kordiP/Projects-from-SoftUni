bd58215bd923f15a74ef455c0675aee8d1f1c765
kordiP <nasikordov@gmail.com>
2022-05-21 23:58:33 +0300

    Added 1-cycle --> Prints all numbers from first to second inputed

---
+++ 1-cycle/1 cycle/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Vuvedete 2 chisla: ");
+int s = int.Parse(Console.ReadLine());
+int m = int.Parse(Console.ReadLine());
+int a = m - s;
+int k = Math.Abs(a);
+for (int i = 0; i <= k; i++)
+{
+    Console.Write(s);
+    s = s + a / k;
+}
