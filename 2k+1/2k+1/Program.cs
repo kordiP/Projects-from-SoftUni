6f68069df8d2fd43cdef34eb9307d2eaac558cdd
kordiP <nasikordov@gmail.com>
2022-05-23 14:11:23 +0300

    Added 2k+1 --> Makes row of numbers from 1 to the given number.

---
+++ 2k+1/2k+1/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2k_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Въведи крайно число за редицата:");
+            int i = int.Parse(Console.ReadLine());
+            int p = 1;
+            while (p<=i)
+            {
+                Console.Write(p+" ");
+                p = 2 * p + 1;
+            }
+        }
+    }
+}
---
+++ 2k+1/parole/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace parole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = "";
+            while (password.Length<=6)
+            {
+                Console.WriteLine("Въведи парола (повече от 8 знака)");
+                password = Console.ReadLine(); 
+            }
+            Console.WriteLine("Паролата е записана.");
+        }
+    }
+}
