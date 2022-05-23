1d5dcfca1768f9de5456220a2fe1a40692198eee
kordiP <nasikordov@gmail.com>
2022-05-23 14:35:10 +0300

    Added +,-,0-broqch --> Counts all the zeros, positive and negative numbers inputed, respectively.

---
+++ kordi/kordi/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace kordi
+{
+    class Program
+    {
+        private static readonly object x;
+        private static readonly object y;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("x=");
+            Console.ReadLine("x =");
+            
+            
+            
+
+        }
+    }
+}
