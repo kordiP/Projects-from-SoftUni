700b3dfeb4879e93b7d82de090d14a0d28801b8f
kordiP <nasikordov@gmail.com>
2022-05-24 19:41:33 +0300

    Added 18,12,2020 --> Choose a city, product from a menu and quantity

---
+++ 18,12,2020/18,12,2020/Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace _18_12_2020
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button10 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(194, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(180, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "От кой град ще закупите стока?";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(42, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(144, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Каква стока ще желаете?";
+            this.label2.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(63, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Налични стоки:";
+            this.label3.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(231, 72);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(106, 15);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Налични градове:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(249, 90);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "София";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(249, 151);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Пловдив";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(249, 122);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Варна";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(73, 114);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(99, 23);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Вода";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Visible = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(73, 143);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(99, 23);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "Бира";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Visible = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(73, 172);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(99, 23);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "Фъстъци";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Visible = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(73, 201);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(99, 23);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "Бонбони";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Visible = false;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(73, 85);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "534т";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Visible = false;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(73, 85);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(99, 23);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "Кафе";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Visible = false;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(394, 57);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(206, 15);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "В какво количество ще ги закупите?";
+            this.label5.Visible = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(443, 75);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 10;
+            this.textBox1.Visible = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(444, 147);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(99, 15);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Крайната цена е:";
+            this.label6.Visible = false;
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(455, 104);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(75, 23);
+            this.button10.TabIndex = 12;
+            this.button10.Text = "Поръчай!";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Visible = false;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(798, 343);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = " ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button10;
+    }
+}
+
---
+++ 18,12,2020/18,12,2020/Form1.cs
@@ -0,0 +1,291 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _18_12_2020
+{
+    public partial class Form1 : Form
+    {
+        double kafePL = 0.40;
+        double kafeSF = 0.50;
+        double kafeVR = 0.45;
+        double vodaPL = 0.70;
+        double vodaSF = 0.80;
+        double vodaVR = 0.75;
+        double biraPL = 1.15;
+        double biraSF = 1.20;
+        double biraVR = 1.10;
+        double fustukPL = 1.50;
+        double fustukSF = 1.60;
+        double fustukVR = 1.55;
+        double bonbonPL = 1.30;
+        double bonbonSF = 1.45;
+        double bonbonVR = 1.35;
+        string grad;
+        string produkt;
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+      
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            //софия
+            label5.Visible = false;
+            label2.Visible = true;
+            label3.Visible = true;
+            button1.Visible = false;
+            button2.Visible = false;
+            button3.Visible = false;
+            label1.Visible = false;
+            label4.Visible = false;
+            button9.Visible = true;
+            button5.Visible = true;
+            button6.Visible = true;
+            button7.Visible = true;
+            button8.Visible = true;
+            button9.Text = "Кафе - 0,50 лв."; 
+            button5.Text = "Вода - 0,80 лв.";
+            button6.Text = "Бира - 1,20 лв.";
+            button7.Text = "Фъстъци - 1,60 лв.";
+            button8.Text = "Бонбони - 1,45 лв.";
+            grad = "S";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //варна
+            label5.Visible = false;
+            label2.Visible = true;
+            label3.Visible = true;
+            button1.Visible = false;
+            button2.Visible = false;
+            button3.Visible = false;
+            label1.Visible = false;
+            label4.Visible = false;
+            button9.Visible = true;
+            button5.Visible = true;
+            button6.Visible = true;
+            button7.Visible = true;
+            button8.Visible = true;
+            button9.Text = "Кафе - 0,45 лв.";
+            button5.Text = "Вода - 0,75 лв.";
+            button6.Text = "Бира - 1,10 лв.";
+            button7.Text = "Фъстъци - 1,55 лв.";
+            button8.Text = "Бонбони - 1,35 лв.";
+            grad = "V";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //пловдив
+            label5.Visible = false;
+            label2.Visible = true;
+            label3.Visible = true;
+            button1.Visible = false;
+            button2.Visible = false;
+            button3.Visible = false;
+            label1.Visible = false;
+            label4.Visible = false;
+            button9.Visible = true;
+            button5.Visible = true;
+            button6.Visible = true;
+            button7.Visible = true;
+            button8.Visible = true;
+            button9.Text = "Кафе - 0,40 лв.";
+            button5.Text = "Вода - 0,70 лв.";
+            button6.Text = "Бира - 1,15 лв.";
+            button7.Text = "Фъстъци - 1,50 лв.";
+            button8.Text = "Бонбони - 1,30 лв.";
+            grad = "P";
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            //za kafe
+            label2.Visible = false;
+            label3.Visible = false;
+            button5.Visible = false;
+            button6.Visible = false;
+            button7.Visible = false;
+            button8.Visible = false;
+            button9.Visible = false;
+            label5.Visible =true;
+            textBox1.Visible =true;
+            button10.Visible = true;
+            produkt = "k";
+            
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            //za voda
+            label2.Visible = false;
+            label3.Visible = false;
+            button5.Visible = false;
+            button6.Visible = false;
+            button7.Visible = false;
+            button8.Visible = false;
+            button9.Visible = false;
+            label5.Visible = true;
+            textBox1.Visible = true;
+            button10.Visible = true;
+            produkt = "v";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            //za bira
+            label2.Visible = false;
+            label3.Visible = false;
+            button5.Visible = false;
+            button6.Visible = false;
+            button7.Visible = false;
+            button8.Visible = false;
+            button9.Visible = false;
+            label5.Visible = true;
+            textBox1.Visible = true;
+            button10.Visible = true;
+            produkt = "bira";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            //za fustuci
+            label2.Visible = false;
+            label3.Visible = false;
+            button5.Visible = false;
+            button6.Visible = false;
+            button7.Visible = false;
+            button8.Visible = false;
+            button9.Visible = false;
+            label5.Visible = true;
+            textBox1.Visible = true;
+            button10.Visible = true;
+            produkt = "f";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            //za bonboni
+            label2.Visible = false;
+            label3.Visible = false;
+            button5.Visible = false;
+            button6.Visible = false;
+            button7.Visible = false;
+            button8.Visible = false;
+            button9.Visible = false;
+            label5.Visible = true;
+            textBox1.Visible = true;
+            button10.Visible = true;
+            produkt = "b";
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            label6.Visible =true;
+            int kol = int.Parse(textBox1.Text);
+                  if (kol%2!=0)
+                  {
+                       textBox1.Text = "Не продаваме продуктите си на части!";
+                  }
+            double sum;
+            if (grad=="P")
+            {
+                if (produkt=="k")
+                {
+                    sum = kol * kafePL;
+                    label6.Text ="Крайната цена е " + sum + "0 лв.";
+                }
+                if (produkt == "v")
+                {
+                    sum = kol * vodaPL;
+                    label6.Text = "Крайната цена е " + sum + "0 лв.";
+                }
+                if (produkt == "bira")
+                {
+                    sum = kol * biraPL;
+                    label6.Text = "Крайната цена е " + sum + "0 лв.";
+                }
+                if (produkt == "f")
+                {
+                    sum = kol * fustukPL;
+                    label6.Text = "Крайната цена е " + sum + "0 лв.";
+                }
+                if (produkt == "b")
+                {
+                    sum = kol * bonbonPL;
+                    label6.Text = "Крайната цена е " + sum + "0 лв.";
+                }
+
+            }
+            
+            if (grad == "S")
+            {
+                if (produkt == "k")
+                {
+                    sum = kol * kafeSF;
+                    label6.Text = "Крайната цена е " + sum + "0 лв.";
+                }
+                if (produkt == "v")
+                {
+                    sum = kol * vodaSF;
+                    label6.Text = "Крайната цена е " + sum + "0 лв.";
+                }
+                if (produkt == "bira")
+                {
+                    sum = kol * biraSF;
+                    label6.Text = "Крайната цена е " + sum + "0 лв.";
+                }
+                if (produkt == "f")
+                {
+                    sum = kol * fustukSF;
+                    label6.Text = "Крайната цена е " + sum + "0 лв.";
+                }
+                if (produkt == "b")
+                {
+                    sum = kol * bonbonSF;
+                    label6.Text = "Крайната цена е " + sum + "0 лв.";
+                }
+            }
+            if (grad == "V")
+            {
+                if (produkt == "k")
+                {
+                    sum = kol * kafeVR;
+                    label6.Text = "Крайната цена е " + sum + "0 лв.";
+                }
+                if (produkt == "v")
+                {
+                    sum = kol * vodaVR;
+                    label6.Text = "Крайната цена е " + sum + "0 лв.";
+                }
+                if (produkt == "bira")
+                {
+                    sum = kol * biraVR;
+                    label6.Text = "Крайната цена е " + sum + "0 лв.";
+                }
+                if (produkt == "f")
+                {
+                    sum = kol * fustukVR;
+                    label6.Text = "Крайната цена е " + sum + "0 лв.";
+                }
+                if (produkt == "b")
+                {
+                    sum = kol * bonbonVR;
+                    label6.Text = "Крайната цена е " + sum + "0 лв.";
+                }
+            }
+
+        }
+    }
+}
