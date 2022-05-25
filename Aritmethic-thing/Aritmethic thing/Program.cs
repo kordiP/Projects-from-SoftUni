f1ef067fb2abfbf9fc5a647a83388affbacdeb41
kordiP <nasikordov@gmail.com>
2022-05-25 23:44:04 +0300

    Added Aritmethic-thing --> Finds greatest common divisor

---
+++ Aritmethic-thing/Aritmethic thing/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Aritmethic_thing
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int originalNumber = n;
+            int p = 2;
+            while (n > 1) 
+            {
+                while (n % p == 0)
+                {
+                    Console.Write(p);
+                    if (n == p && n == originalNumber) 
+                        Console.Write(" е просто число.");
+                    Console.WriteLine();
+                    n /= p;
+                }
+                p++;
+            }
+        }
+    }
+}
