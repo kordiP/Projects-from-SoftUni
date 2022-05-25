0dfa4727a52c94c0eb8c1cd797c37dcc88404681
kordiP <nasikordov@gmail.com>
2022-05-25 23:45:38 +0300

    Added banica --> Calculates price for given number of banicas

---
+++ banica/banica/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace banica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double c;
+            Console.Write("Vuvedi cena na edna banica: ");
+            double cena = double.Parse(Console.ReadLine());           
+            Console.Write("Kolko banici iskate da kupite? ");
+            int banica = int.Parse(Console.ReadLine());
+            double lev = (banica * cena);
+            
+            c = Math.Floor(lev);
+            double stotinki = (lev-c)*100;
+            Console.WriteLine("Trqbvat {0} leva i {1} stotinki" ,c,stotinki);
+
+        }
+    }
+}
