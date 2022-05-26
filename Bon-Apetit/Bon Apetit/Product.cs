1a0a824eb3765d7e3b0f98b173bc8d9ebadf1661
kordiP <nasikordov@gmail.com>
2022-05-26 13:18:52 +0300

    Added Bon-Apetit --> Has classes for meals, products for meals and main. Acts as a list of restaurants, meals and products. Can print, add, remove, rename and much more!

---
+++ Bon-Apetit/Bon Apetit/Product.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bon_Apetit
+{
+    internal class Product
+    {
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set 
+            {
+                if (value.Length > 3)
+                    name = value;
+                else
+                    Console.WriteLine("Invalid Command!");
+            }
+        }
+        private double price;
+        public double Price
+        {
+            get { return price; }
+            set 
+            {
+                if (value >= 0.01)
+                    price = value;
+                else
+                    Console.WriteLine("Invalid Command!");
+            }
+        }
+        private int weight;
+        public int Weight
+        {
+            get { return weight; }
+            set 
+            {
+                if (value > 0)
+                    weight = value;
+                else Console.WriteLine("Invalid Command!");
+            }
+        }
+        public Product(string name, double price, int weight)
+        {
+            Name = name;
+            Price = price;
+            Weight = weight;
+        }
+        public static Product GetCheapestProduct(Dictionary<string, Product> products)
+        {
+            return products.OrderBy(x => x.Value.Price).First().Value;
+        }
+
+        public override string ToString()
+        {
+            return $"{name} - {weight}";
+        }
+    }
+}
