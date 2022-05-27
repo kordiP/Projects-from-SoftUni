94dd3455ff9aaf20d7f39bf506359a7bbf2d3f8c
kordiP <nasikordov@gmail.com>
2022-05-27 23:01:39 +0300

    Added calc --> Simple add/subtract calculator.

---
+++ broqch/poloj, otric i nula broqch/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace poloj__otric_i_nula_broqch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int poloj = 0;
+            int otric = 0;
+            int nuli = 0;
+            string chislo = Console.ReadLine();
+            while (chislo != "end")
+            {
+                int chisloto = int.Parse(chislo);
+                if (chisloto < 0)
+                    otric++;
+                else if (chisloto > 0)
+                    poloj++;
+                else if (chisloto == 0)
+                    nuli++;
+                chislo = Console.ReadLine();
+            }
+            Console.WriteLine("Broi na polojitelni: " + poloj);
+            Console.WriteLine("Broi na otricatelni: " + otric);
+            Console.WriteLine("Broi na nuli: " + nuli);
+        }
+    }
+}
---
+++ calc/calc/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calc
+{
+    public partial class Form1 : Form
+    {
+        int a, b, c;
+        public Form1()
+        {
+            
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            a = int.Parse(textBox1.Text);
+            b = int.Parse(textBox2.Text);
+            c = a + b;
+            label1.Text = c.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "0";
+            textBox2.Text = "0";
+            label1.Text = "0";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            a = int.Parse(textBox1.Text);
+            b = int.Parse(textBox2.Text);
+            c = a - b;
+            label1.Text = c.ToString();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
