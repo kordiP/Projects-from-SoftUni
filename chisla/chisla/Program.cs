6b8f9a6da5de2cbd0734898b032ce837c93849b0
kordiP <nasikordov@gmail.com>
2022-05-28 22:25:19 +0300

    Added chisla --> Splits a 2-digit number into 2 seperate ones.

---
+++ chisla/chisla/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace chisla
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            int d = x / 10;
+            int e = x % 10;
+            Console.WriteLine(d);
+            Console.WriteLine(e);
+        }
+    }
+}
