358d0b9ec140bf72ec8e579f8d2ce4aca4dfe96d
kordiP <nasikordov@gmail.com>
2022-05-28 22:25:59 +0300

    Added chetni --> Finds whether a number is even or odd.

---
+++ chetni/chetni/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chetni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            if (n%2==0)
+            {
+                Console.WriteLine("even");
+            }
+            else
+            {
+                Console.WriteLine("odd");
+            }
+        }
+    }
+}
