0c7ab74c4dc121c419b8b27840376849002f72de
kordiP <nasikordov@gmail.com>
2022-05-29 09:10:56 +0300

    Added cikli --> Prints all of 5's divisors in the range 0-156.

---
+++ cikli/cikli/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cikli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int suma=0;
+            for (int i = 1; i <= 156; i++)
+            {
+                if (i%5==0)
+                {
+                    Console.WriteLine(i);
+                }
+                
+                //suma=suma+i;       
+            }
+            //Console.WriteLine(suma);
+        }
+    }
+}
