f9f1b6a4da4e51cdc7a164e904040a1e008f054f
kordiP <nasikordov@gmail.com>
2022-05-31 01:36:48 +0300

    Added ConsoleApp3 --> Finds whether a word contains itself inside another word, and prints at which index it starts and ends.

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,21 @@
+﻿                                                                                                                                   using System;
+
+namespace niz_upr
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1= Console.ReadLine();
+            string s2 = Console.ReadLine();
+            int i = s1.IndexOf(s2);
+            if (i < 0) Console.WriteLine("ne se sadurja");
+            else Console.WriteLine("sadarja se");
+            Console.WriteLine(s1.IndexOf(s2));
+            Console.WriteLine(s1[0]);
+            int x = s1.Length;
+            Console.WriteLine(s1[x-1]);
+        }
+    }
+}
