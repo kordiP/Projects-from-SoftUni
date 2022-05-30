901e37d07747251ef2b492d6e9ab5a791f408f3e
kordiP <nasikordov@gmail.com>
2022-05-31 01:34:31 +0300

    Added ConsoleApp2 --> Test program for fields in Main().

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GG
+{
+    class Program
+    {
+        private static object x;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("x=");
+            Console.ReadLine(x);
+        }
+    }
+}
