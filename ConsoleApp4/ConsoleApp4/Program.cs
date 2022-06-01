0db6fc2a030f768e26a1ebb038a1ce811ab3e1d8
kordiP <nasikordov@gmail.com>
2022-06-01 23:50:11 +0300

    Added ConsoleApp4 --> Splits a given word it 3 equal parts.

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            string rev1 = name.Substring(0, 1);       
+            string rev2 = name.Substring(1, 1);
+            string rev31 = name.Substring(2,1);
+            Console.WriteLine(rev31 + rev2 + rev1);
+            
+           
+             
+        }
+    }
+}
