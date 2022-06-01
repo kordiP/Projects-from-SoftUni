373ad59050db9eb099091dcc8f5d895c82c119d8
kordiP <nasikordov@gmail.com>
2022-06-01 23:51:54 +0300

    Added cqlo4 --> Optimized way of finding whether a number is positive, negative or zero.

---
+++ cqlo4/cqlo4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace cqlo4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            if (x > 0) { Console.WriteLine("polojitelno"); }
+            else
+            {
+                if (x == 0) { Console.WriteLine("nula"); }
+                else { Console.WriteLine("otricatelno"); }
+
+            }
+        }
+    }
+}
