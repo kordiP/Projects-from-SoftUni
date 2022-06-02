0a63c9ab5f0594ca5d1feca57c7a2f9c08af946b
kordiP <nasikordov@gmail.com>
2022-06-02 19:37:20 +0300

    Added cvetq --> A dividing-only calculator

---
+++ cvetq/cvetq/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cvetq
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a = int.Parse(textBox1.Text);
+            int b = int.Parse(textBox2.Text);
+            int c = b/a ;
+            label1.Text = c.ToString();
+
+        }
+    }
+}
