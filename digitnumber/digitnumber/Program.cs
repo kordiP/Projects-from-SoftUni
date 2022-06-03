e5657bc2fcfda1cf4cfa55059718f7a8da2cd910
kordiP <nasikordov@gmail.com>
2022-06-03 23:46:53 +0300

    Added digitnumber --> Converts digit from integer to word.

---
+++ digitnumber/digitnumber/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace digitnumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            if (x == 0)
+            {
+                Console.WriteLine("zero");
+            }
+            else if (x == 1)
+            {
+                Console.WriteLine("one");
+            }
+            else if (x == 2)
+            {
+                Console.WriteLine("two");
+            }
+            else if (x == 3)
+            {
+                Console.WriteLine("three");
+            }
+            else if (x == 4)
+            {
+                Console.WriteLine("four");
+            }
+            else if (x == 5)
+            {
+                Console.WriteLine("five");
+            }
+            else if (x == 6)
+            {
+                Console.WriteLine("six");
+            }
+            else if (x == 7)
+            {
+                Console.WriteLine("seven");
+            }
+           else if (x == 8)
+            {
+                Console.WriteLine("eight");
+            }
+           else if (x == 9)
+            {
+                Console.WriteLine("nine");
+            }
+            else
+            {
+                Console.WriteLine("not a 1-digit number");
+            }
+        }
+        }
+    }
+
+
