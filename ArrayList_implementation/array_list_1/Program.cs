4f03702187a237038bb207255e0cff72d6f7b440
kordiP <nasikordov@gmail.com>
2022-06-04 23:54:51 +0300

    Added ArrayList_imlpementation --> Implements ArrayList

---
+++ ArrayList_implementation/array_list_1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+
+namespace array_list_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList list = new ArrayList();
+            list.Add("Hello");
+            list.Add(5);
+            list.Add(3.14159);
+            list.Add(DateTime.Now);
+
+            for (int i=0;i<list.Count;i++)
+            {
+                object value = list[i];
+                Console.WriteLine("Index={0}; Value={1}\n", i, value); ;
+            }
+
+        }
+    }
+}
