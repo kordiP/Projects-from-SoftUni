4eda3965d41d0ba63fa87fa4e0b53de38e0e5923
kordiP <nasikordov@gmail.com>
2022-06-05 23:24:49 +0300

    Added списък --> Has a list that you can add things to, sort them or remove things.

---
+++ списък/dr_chisla/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace dr_chisla
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Брой елементи в списъка n= ");
+            int n = int.Parse(Console.ReadLine());
+
+            List<double> chisla = new List<double>();
+            Console.WriteLine("Елементи на списъка (дробни числа):");
+            for(int i=0;i<n;i++)
+            {
+                Console.Write("Елемент {0} = ", i);
+                chisla.Add(double.Parse(Console.ReadLine()));
+            }
+            Console.WriteLine("Списъкът е:");
+            foreach(double x in chisla)
+                Console.WriteLine(x);
+
+            Console.WriteLine("Сортиран списък - възходящ ред:");
+            chisla.Sort();
+            foreach (double x in chisla)
+                Console.WriteLine(x);
+
+            Console.WriteLine("Сортиран списък - низходящ ред:");
+            chisla.Reverse();
+            foreach (double x in chisla)
+                Console.WriteLine(x);
+
+            Console.WriteLine("Минимален елемент в списъка Min= "+chisla.Min());
+            Console.WriteLine("Максимален елемент в списъка Min= " + chisla.Max());
+
+        }
+    }
+}
---
+++ списък/списък/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+namespace списък
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> myList = new List<int>();
+            myList.Add(5);
+            myList.Add(1);
+            myList.Add(8);
+            myList.Add(-7);
+            //myList.RemoveAt(1);
+
+            Console.WriteLine("Elements in myList is: ");
+            for (int i = 0; i < myList.Count; i++)
+            {
+                Console.Write(myList[i] + " ");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Sort myList is: ");
+            myList.Sort();
+            for (int i = 0; i < myList.Count; i++)
+            {
+                Console.Write(myList[i] + " ");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Reverse myList is:");
+            myList.Reverse();
+            for (int i = 0; i < myList.Count; i++)
+            {
+                Console.Write(myList[i] + " ");
+            }
+        }
+    }
+}
