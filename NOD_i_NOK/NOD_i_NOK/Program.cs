b26d551fabc2f347fe936ed224443f18db01e26a
kordiP <nasikordov@gmail.com>
2022-06-07 23:33:31 +0300

    Added NOD_i_NOK --> Finds greatest common divisor and the least total multiple(it think).

---
+++ NOD_i_NOK/NOD_i_NOK/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace NOD_i_NOK
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int d = 2;
+            int nod = 1;
+            int nok = 1;
+
+            while (a > 1 || b > 1)
+            {
+                while (a % d == 0 || b % d == 0)
+                {
+                    if (a % d == 0 && b % d == 0)
+                    {
+                        nod *= d;
+                        a /= d;
+                        b /= d;
+                    }
+                    else if (a % d == 0)
+                        a /= d;
+                    else
+                        b /= d;
+                    nok *= d;
+                }
+                d++;
+            }
+            Console.WriteLine(nod);
+            Console.WriteLine(nok);
+        }
+    }
+}
