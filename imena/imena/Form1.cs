6368f91fc6dd503af33b897ce11d104b1c29c100
kordiP <nasikordov@gmail.com>
2022-06-21 22:44:09 +0300

    Added imena --> Changes 2 names that are entered.

---
+++ imena/imena/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace imena
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string s1 = textBox1.Text;
+            string nameNew = textBox2.Text;
+            string nameOld = textBox3.Text;
+           string result= s1.Replace(nameOld,nameNew);
+            textBox3.Text=result;
+        }
+    }
+}
