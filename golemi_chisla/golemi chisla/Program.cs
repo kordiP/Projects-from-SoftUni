44d965a2786f3efdf866c23fcad96cfb3d930e2d
kordiP <nasikordov@gmail.com>
2022-06-21 22:42:19 +0300

    Added golemi_chisla --> Has the ability to add numbers without any limits.

---
+++ golemi_chisla/golemi chisla/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace golemi_chisla
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            if (x / 2 == 1) { Console.WriteLine("da"); }
+           else { Console.WriteLine("ne"); }
+        }
+    }
+}
