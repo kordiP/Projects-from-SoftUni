b1db4fc2fa056deb8291b46528015bc62e14408a
kordiP <nasikordov@gmail.com>
2022-06-22 23:16:24 +0300

    Added imeto --> Greets user by inputed age and name.

---
+++ imeto/imeto/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace imeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("На колко години си?");
+            string godini = Console.ReadLine();
+            Console.Write("Как се казваш?");
+            string name = Console.ReadLine();           
+            Console.WriteLine("Вие сте на {0} години и се казвате {1}.",godini,name);
+        }
+    }
+}
