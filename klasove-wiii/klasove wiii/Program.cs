f188de982483e6e59bfc0b307b860bf66dc2fd76
kordiP <nasikordov@gmail.com>
2022-06-23 22:24:18 +0300

    Added klasove-wiii --> Class of products in a dictionary.

---
+++ klasove-wiii/klasove wiii/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+namespace supermarket
+{
+    class Product
+    {
+        public string name;
+        public double price;
+        public int quantity;
+        public Product()
+        {
+
+        }
+        public Product(string name, double price, int quantity)
+        {
+
+        }
+    }
+    internal class Program
+    {
+        static void Main()
+        {
+            Dictionary<string, Product> products = new Dictionary<string, Product>();
+
+            Product product1 = new Product();
+            product1.name = "Beer";
+            product1.price = 1.50;
+            product1.quantity = 1;
+            products.Add(product1.name, product1);
+
+            Product product2 = new Product("Cheese", 3.80, 2);
+        }
+    }
+}
