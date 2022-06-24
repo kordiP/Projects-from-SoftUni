e9db02f111db8ecfeb868d66dd6a57f7955fe12d
kordiP <nasikordov@gmail.com>
2022-06-24 23:55:16 +0300

    Added kvadranti --> Based on given coordinates, returns in which quadrant is the point with those coordinates in a coordinate system.

---
+++ kvadranti/kvadranti/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace kvadranti
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+            if (x >= 0 && y >= 0) { Console.WriteLine("kvadrant 1"); }
+            if (x <= 0 && y >= 0) { Console.WriteLine("kvadrant 2"); }
+            if (x <= 0 && y <= 0) { Console.WriteLine("kvadrant 3"); }
+            if (x >= 0 && y <= 0) { Console.WriteLine("kvadrant 4"); }
+            if (x == 0) { Console.WriteLine("ednata tochka leji na abscisata"); }
+            if (y == 0) { Console.WriteLine("ednata tochka leji na ordinatata"); }
+        }
+    }
+}
