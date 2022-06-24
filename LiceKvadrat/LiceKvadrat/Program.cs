d051b200b10fd3b703436be63b5d2170fab0516e
kordiP <nasikordov@gmail.com>
2022-06-25 00:10:51 +0300

    Added LiceKvadrat --> Calculates square's area with math function .Pow().

---
+++ LiceKvadrat/LiceKvadrat/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace LiceKvadrat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+	    int S = Math.Pow(a, 2);
+     	    Console.WriteLine("S = " + S + "cm2");
+        }
+    }
+}
