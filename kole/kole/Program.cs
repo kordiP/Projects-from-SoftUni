ae7f3e0f2b10f8b871d57a75957575aae521843d
kordiP <nasikordov@gmail.com>
2022-06-24 23:48:07 +0300

    Added kole --> Use of concatination in writeline.

---
+++ kole/kole/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string k="kole";
+            Console.WriteLine(k+k+k);
+        }
+    }
+}
