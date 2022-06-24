7d1865377a5d3a59b845b9d5bbebcfddbeca4c35
kordiP <nasikordov@gmail.com>
2022-06-25 00:01:39 +0300

    Added labels --> Claculates 2 formulas and outputs them in 2 labels.

---
+++ labels/labels/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace labels
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            int a = 0, c = 12;
+            int b = 10, d = 5;
+            a = c % d;
+            b = b / a;
+            label1.Text = a.ToString();
+            label2.Text = b.ToString();
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
