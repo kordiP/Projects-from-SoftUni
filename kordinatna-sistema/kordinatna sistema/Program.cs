0c4507a58c3f5b9786042b465fc4c2b63ca27b39
kordiP <nasikordov@gmail.com>
2022-06-24 23:51:36 +0300

    Added kordinatna-sistema --> Checks whether a triangle exists in a given coordinates of a given space.

---
+++ kordinatna-sistema/kordinatna sistema/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kordinatna_sistema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, x1, x2, y1, y2;
+            Console.WriteLine("Vuvedete stoinostite na a, x1, x2, b, y1, y2");
+            a = double.Parse(Console.ReadLine());
+            x1 = double.Parse(Console.ReadLine());
+            x2 = double.Parse(Console.ReadLine());
+            b = double.Parse(Console.ReadLine());
+            y1 = double.Parse(Console.ReadLine());
+            y2 = double.Parse(Console.ReadLine());
+            if (a==x1 || a==x2 || b==y1 || b==y2)
+            {
+                Console.WriteLine("Leji na strana");
+            }
+            else
+            {
+                Console.WriteLine("Ne leji na strana"); 
+            }
+        }
+    }
+}
