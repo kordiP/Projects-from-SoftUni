c47097f5e9d7b442d6d8de11289f4405b89fc612
kordiP <nasikordov@gmail.com>
2022-06-27 23:57:29 +0300

    Added max --> Finds the lowest number of given ones.

---
+++ max/max/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace max
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Брой числа?");
+            int numba = int.Parse(Console.ReadLine());
+            Console.WriteLine("Напиши числата едно по едно:");
+            int min = int.MaxValue;
+            for (int i = 0; i < numba; i++)
+            {
+                int c = int.Parse(Console.ReadLine());
+                if (min>c)
+                {
+                    min=c;
+                }
+            }
+            Console.WriteLine(min);
+        }
+    }
+}
