a9f421524ad69c8ab093fbb38855e4683c3470eb
kordiP <nasikordov@gmail.com>
2022-06-27 23:56:34 +0300

    Added matrix_assignment --> Builds a matrix and prints its' elements.

---
+++ matrix_assignment/matrix_assignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace matrix_assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = int.Parse(Console.ReadLine());
+            int columns = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[rows, columns];
+            for (int i = 0; i < rows - 1; i++) 
+                for (int j = 0; j < columns - 1; j++) 
+                    matrix[rows, columns] = int.Parse(Console.ReadLine());
+            for (int i = 0; i < rows - 1; i++)
+            {
+                for (int j = 0; j < columns - 1; j++) 
+                    Console.Write(matrix[rows, columns] + " ");
+                Console.WriteLine();
+            }
+        }
+    }
+}
