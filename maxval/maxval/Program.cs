450641f5650dc16b25ae8ddaf0300871b9ee2fae
kordiP <nasikordov@gmail.com>
2022-06-28 22:07:24 +0300

    Added maxval --> Gets the sum of written numbers.

---
+++ maxval/maxval/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace maxval
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n; int suma = 0;
+            Console.WriteLine("Vavedi broi chisla");
+            n = int.Parse(Console.ReadLine());
+           
+            for (int i = 0; i < n; i++)
+            {
+ int x = int.Parse(Console.ReadLine());
+                suma = suma + x;
+            }
+            Console.WriteLine(suma);
+
+        }
+    }
+}
