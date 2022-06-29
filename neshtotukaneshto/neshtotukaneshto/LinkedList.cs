4966c428ba5f51a847253b6287b5e3f4558f21a4
kordiP <nasikordov@gmail.com>
2022-06-29 23:32:04 +0300

    Added neshtotukaneshto --> Linked list self-implementation.

---
+++ neshtotukaneshto/neshtotukaneshto/LinkedList.cs
@@ -0,0 +1,207 @@
+﻿using System;
+
+namespace neshtotukaneshto
+{
+    class LinkedList<T>
+    {
+        private class ListNode
+        {
+            public T Element { get; set; }
+            public ListNode NextNode { get; set; }
+            public ListNode(T element, ListNode prevNode)
+            {
+                this.Element = element;
+                prevNode.NextNode = this;
+            }
+            public ListNode(T element)
+            {
+                this.Element = element;
+                this.NextNode = null;
+            }
+        }
+        private ListNode head;
+        private ListNode tail;
+        public int count;
+        public LinkedList()
+        {
+            this.head = null;
+            this.tail = null;
+            this.count = 0;
+        }
+        public void Add(T item)
+        {
+            if (this.head == null)
+            {
+                this.head = new ListNode(item);
+                this.tail = this.head;
+            }
+            else
+            {
+                ListNode newNode = new ListNode(item, tail);
+                this.tail = newNode;
+            }
+            this.count++;
+        }
+        public void Insert(T item, int index)
+        {
+            if (index < 0 || index > count + 1) throw new ArgumentOutOfRangeException("Invalid index: " + index);
+            if (count == 0)
+            {
+                head = new ListNode(item);
+                tail = head;
+             }
+            else
+            {
+                int currentIndex = 0;
+                ListNode currentNode = head;
+                ListNode prevNode = null;
+                ListNode itemToAdd = new ListNode(item);
+                while (currentIndex < index)
+                {
+                    prevNode = currentNode;
+                    currentNode = currentNode.NextNode;
+                    currentIndex++;
+                }
+                if (index == 0)
+                {
+                    head = itemToAdd;
+                    itemToAdd.NextNode = currentNode;
+                }
+                else if (index == count)
+                {
+                    prevNode.NextNode = itemToAdd;
+                    tail = itemToAdd;
+                }
+                else
+                {
+                    prevNode.NextNode = itemToAdd;
+                    itemToAdd.NextNode = currentNode;
+                }
+            }
+            count++;
+        }
+        public void AddSpecial(T item)
+        {
+            Insert(item, 0);
+        }
+        public T RemoveAt(int index)
+        {
+            if (index >= this.count || index < 0)
+            {
+                throw new ArgumentOutOfRangeException("Invalid index: " + index);
+            }
+            int currentIndex = 0;
+            ListNode currentNode = this.head;
+            ListNode prevNode = null;
+            while (currentIndex < index)
+            {
+                prevNode = currentNode;
+                currentNode = currentNode.NextNode;
+                currentIndex++;
+            }
+            RemoveListNode(currentNode, prevNode);
+            return currentNode.Element;
+        }
+        private void RemoveListNode(ListNode node, ListNode prevNode)
+        {
+            this.count--;
+            if (this.count == 0)
+            {
+                this.head = null;
+                this.tail = null;
+            }
+            else if (prevNode == null)
+            {
+                this.head = node.NextNode;
+            }
+            else
+            {
+                prevNode.NextNode = node.NextNode;
+            }
+            if (object.ReferenceEquals(this.tail, node))
+            {
+                this.tail = prevNode;
+            }
+        }
+        public int Remove(T item)
+        {
+            int currentIndex = 0;
+            ListNode currentNode = this.head;
+            ListNode prevNode = null;
+            while (currentNode != null)
+            {
+                if (object.Equals(currentNode.Element, item))
+                {
+                    break;
+                }
+                prevNode = currentNode;
+                currentNode = currentNode.NextNode;
+                currentIndex++;
+            }
+            if (currentNode != null)
+            {
+                this.RemoveListNode(currentNode, prevNode);
+                return currentIndex;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+        public int IndexOf(T item)
+        {
+            int index = 0;
+            ListNode currentNode = this.head;
+            while (currentNode != null)
+            {
+                if (object.Equals(currentNode.Element, item))
+                {
+                    return index;
+                }
+                currentNode = currentNode.NextNode;
+                index++;
+            }
+            return -1;
+        }
+        public bool Contains(T item)
+        {
+            int index = this.IndexOf(item);
+            bool found = (index != -1);
+            return found;
+        }
+        public T this[int index]
+        {
+            get
+            {
+                if (index >= this.count || index < 0)
+                {
+                    throw new ArgumentOutOfRangeException(
+                    "Invalid index: " + index);
+                }
+                ListNode currentNode = this.head;
+                for (int i = 0; i < index; i++)
+                {
+                    currentNode = currentNode.NextNode;
+                }
+                return currentNode.Element;
+            }
+            set
+            {
+                if (index >= this.count || index < 0)
+                {
+                    throw new ArgumentOutOfRangeException(
+                    "Invalid index: " + index);
+                }
+                ListNode currentNode = this.head;
+                for (int i = 0; i < index; i++)
+                {
+                    currentNode = currentNode.NextNode;
+                }
+                currentNode.Element = value;
+            }
+        }
+
+    }
+}
+
+
---
+++ neshtotukaneshto/neshtotukaneshto/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace neshtotukaneshto
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            LinkedList<string> shoppingList = new LinkedList<string>();
+            shoppingList.Add("Second");
+            shoppingList.Insert("Fourth", 1);
+            shoppingList.Insert("Third", 1); // Goes to index 1, item previously assigned with 1 is now 2
+            shoppingList.AddSpecial("First");
+            Console.WriteLine("We need to buy:");
+            for (int i = 0; i < shoppingList.count; i++)
+            {
+                Console.WriteLine(" - " + shoppingList[i]);
+            }
+        }
+    }
+
+}
