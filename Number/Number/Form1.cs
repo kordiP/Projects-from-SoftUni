797e3d4e11fa87de72382302d2e4a720d9ca21fb
kordiP <nasikordov@gmail.com>
2022-07-01 22:49:48 +0300

    Added Number --> Divides a three-digit number to it's hundreths, tenths and digits.

---
+++ Number/Number/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Number
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int x = int.Parse(textBox1.Text);
+            int suma;
+            int ed = x % 10;
+            int des = x / 10 % 10;
+            int sto = x / 100;
+            suma = ed + des + sto;
+            label1.Text = suma.ToString();
+
+        }
+    }
+}
