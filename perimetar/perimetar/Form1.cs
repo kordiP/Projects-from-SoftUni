8cc0a348be02acbd8b8db01d14ed83feb171acfe
kordiP <nasikordov@gmail.com>
2022-07-02 23:47:49 +0300

    Added perimetar --> Finds square's perimetre and surface area.

---
+++ perimetar/perimetar/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace perimetar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double side = double.Parse(textBox1.Text);
+            double side2 = double.Parse(textBox2.Text);
+            double P = side + side2+side + side2;
+            label3.Text = P.ToString();
+        }
+    }
+}
