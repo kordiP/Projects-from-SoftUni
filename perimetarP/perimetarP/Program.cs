8df122b884af2c96c26243762761645e887b29fb
kordiP <nasikordov@gmail.com>
2022-07-03 23:44:26 +0300

    Added perimetarP --> Finds 1 side of parallelogram when given the other side and the height.

---
+++ perimetarP/perimetarP/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace perimetarP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("a=");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("d=");
+            double d = double.Parse(Console.ReadLine());
+            double b2 = d * d - a * a;
+            double b = Math.Sqrt(b2);
+            double s = a * b;
+            double p = a + a + b + b;
+            Console.WriteLine("s= {0} kv. sm",s);
+            Console.WriteLine("b= {0} sm",b);
+            Console.WriteLine("p= {0} sm",p);
+
+        }
+    }
+}
