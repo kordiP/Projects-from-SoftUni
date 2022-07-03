85b195797a0503295dd89682874d425f0a6f767a
kordiP <nasikordov@gmail.com>
2022-07-03 23:51:18 +0300

    Added Pkvadrat --> Finds sides of square, based on the given perimetre.

---
+++ Pkvadrat/Pkvadrat/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pkvadrat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double P = double.Parse(Console.ReadLine());
+            double a = P / 4;
+            Console.WriteLine(a);
+        }
+    }
+}
