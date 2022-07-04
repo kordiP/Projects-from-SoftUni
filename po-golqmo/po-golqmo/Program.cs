be75a0b0fa77ae6f116187f0845fe9d502ecd1e3
kordiP <nasikordov@gmail.com>
2022-07-04 23:15:27 +0300

    Added po-golqmo --> Outputs the bigger of two given numbers.

---
+++ po-golqmo/po-golqmo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace po_golqmo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+            if (x > y) { Console.WriteLine(x); }
+            else { Console.WriteLine(y); }
+        }
+    }
+}
