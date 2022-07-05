ab933e995155dee7bf7d261ffdf445d2643636df
kordiP <nasikordov@gmail.com>
2022-07-05 23:55:24 +0300

    Added pravougulnici --> Calculates perimetre of a rectangle and has checks for the sides.

---
+++ pravougulnici/pravougulnici/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pravougulnici
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            if (a > 0)
+            {
+                int b = int.Parse(Console.ReadLine());
+                if (b > 0) { int P = a + b + a + b; Console.WriteLine("P={0}",P); }
+                else                 
+                    Console.WriteLine("b...?");
+                }
+            else Console.WriteLine("a...?"); }
+            }
+        }
+    
+
+
