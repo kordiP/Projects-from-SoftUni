640af8b6de7b024dbd97da62874e510c4eab7a7b
kordiP <nasikordov@gmail.com>
2022-07-05 23:53:51 +0300

    Added PokajiSkrii --> Has 2 buttons - to hide and show the windows forms app.

---
+++ PokajiSkrii/PokajiSkrii/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PokajiSkrii
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Visible = false;
+            label1.Visible = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "Enlo";
+            textBox1.Visible = true;
+            label1.Visible = true;
+        }
+    }
+}
