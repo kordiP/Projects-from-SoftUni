fce6f27df66f00fb9b332f86c2fe539fbb055188
kordiP <nasikordov@gmail.com>
2022-07-06 23:50:48 +0300

    Added pravougulnik-kvadranti --> Checks whether a rectangle is inside of a given field of a coordinate system.

---
+++ pravougulnik-kvadranti/pravougulnik vav kvadranti/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pravougulnik_vav_kvadranti
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            
+            var x= double.Parse(Console.ReadLine());
+            var y= double.Parse(Console.ReadLine());
+            var x1 = double.Parse(Console.ReadLine());
+            var y1 = double.Parse(Console.ReadLine());
+            var x2 = double.Parse(Console.ReadLine());
+            var y2 = double.Parse(Console.ReadLine());
+            if (x1<=x&&x<=x2&&y1<=y&&y<=y2)
+            {
+                Console.WriteLine("inside");
+            }
+            else Console.WriteLine("outside!");
+        }
+    }
+}
