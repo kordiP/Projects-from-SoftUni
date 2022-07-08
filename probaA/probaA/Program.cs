c5554a59e07194d455897545ce5bcb2ed0633aed
kordiP <nasikordov@gmail.com>
2022-07-08 23:35:32 +0300

    Added probaA --> Testing to see how adding works.

---
+++ probaA/probaA/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace probaA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 2;
+            Console.WriteLine(++a);
+            Console.WriteLine(a);
+        }
+    }
+}
