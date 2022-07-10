1fa1100b80457039b42e074f5dbb32be898b034d
kordiP <nasikordov@gmail.com>
2022-07-10 22:52:00 +0300

    Added prodaljenia-for --> Draws a square made out of star, to which's size is determined by the user.

---
+++ prodaljenia-for/prodaljenia-for/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prodaljenia_for
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vavedi n:");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                Console.Write("*");
+
+            }
+            Console.WriteLine();
+            for (int i = 1; i <= n-2; i++)
+            {
+                Console.Write("*");
+                for (int j = 1; j < n-1; j++)
+                {
+                    Console.Write(" ");
+                }
+                Console.Write("*");
+                Console.WriteLine();
+            }
+            for (int i = 1; i <= n; i++)
+            {
+                Console.Write("*");
+
+            }
+            Console.WriteLine();
+        }
+    }
+}
