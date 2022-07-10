975f13f6eddf23a7b35d5cf25ccbdc129a244326
kordiP <nasikordov@gmail.com>
2022-07-10 22:52:41 +0300

    Added proverka --> A simple add test.

---
+++ proverka/proverka/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proverka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for (int i = 0; i < n; i=i+2)
+            {
+                sum = sum + 2;
+
+            }
+            Console.WriteLine(sum);
+
+        }
+    }
+}
