95451f86764120fc43b604265875949e7eb7c61d
kordiP <nasikordov@gmail.com>
2022-07-15 23:47:12 +0300

    Added razelqne --> Splits a multi-digit number to it's hundreths, tenths and numbers

---
+++ razelqne/razelqne/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace razelqne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int chislo = int.Parse(Console.ReadLine());
+            //int m = chislo / 10;
+            //int d = m % 10;
+            //int parvo = chislo / 100;
+            //int vtoro = d;
+            //int treto = chislo % 10;
+            //Console.WriteLine(parvo);
+            //Console.WriteLine(vtoro);
+            //Console.WriteLine(treto);
+            Console.WriteLine("V kolko chasa izlita?");
+            int h =int.Parse(Console.ReadLine());
+            int m =int.Parse(Console.ReadLine());
+            Console.WriteLine("Kolko chasa i minuti trae?");
+            int h1 =int.Parse(Console.ReadLine());
+            int m1 =int.Parse(Console.ReadLine());
+            int m2 = m + m1;
+            int h2 = h + h1+m2/60;
+            m2 = m2 % 60;
+            h2 = h2 % 24;          
+            Console.WriteLine("Samoletat kaca vav " + h2 + ":" + m2 + ".");
+
+
+
+        }
+    }
+}
