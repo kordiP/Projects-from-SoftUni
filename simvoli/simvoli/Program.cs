5106276ffe04d5ea8a3b813d163f1ce31f24b4fb
kordiP <nasikordov@gmail.com>
2022-07-17 23:44:57 +0300

    Added simvoli --> Small test of code for strings.

---
+++ simvoli/simvoli/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace simvoli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //char parvoSimvolche = 'A';
+            //char vtoroSimvolche = 'b';
+
+
+            //Console.WriteLine((int)parvoSimvolche);
+            //Console.WriteLine((int)vtoroSimvolche);
+            //Console.WriteLine(parvoSimvolche/vtoroSimvolche);
+            //Console.WriteLine((char)12345); 
+            string duma = "hello gosho!                                                                           ";
+            //string duma2 = Console.ReadLine();
+            Console.WriteLine(duma.Trim());
+            Console.WriteLine(duma.Length);
+        }
+    }
+}    
