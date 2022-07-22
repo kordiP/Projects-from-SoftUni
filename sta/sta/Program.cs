54ad845a3b25f4e0263d5383655224fc6233a197
kordiP <nasikordov@gmail.com>
2022-07-22 23:52:53 +0300

    Added sta --> Greets the user with the inserted name.

---
+++ sta/sta/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace sta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hey! Um, whats your name?");
+            string s = Console.ReadLine();
+            Console.WriteLine("Oh, hi " + s + "!");
+        }
+    }
+}
