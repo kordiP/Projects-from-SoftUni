ba04bf00262b732bba1cb8d67d8e14ff2da41f63
kordiP <nasikordov@gmail.com>
2022-07-26 23:47:47 +0300

    Added suma-na-redica --> Sums numbers and outputs the last number.

---
+++ suma-na-redica/suma na redica/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Vuvedete sumata na chislata: ");
+double S = double.Parse(Console.ReadLine());
+double n = 0;
+double k = 1;
+double suma = 0;
+double znak = 1;
+double poreden_chlen= (k / (2 * n + 1));
+suma = suma + (k / (2 * n + 1));
+n++;
+while (poreden_chlen < S)
+{
+    poreden_chlen = (k / (2 * n + 1));
+    znak = -znak;
+    suma = suma + ((k / (2 * n + 1))) * znak;
+    n++;
+    Console.WriteLine(suma);
+}
+Console.WriteLine("Sumata na chislata e: " + suma);
+Console.WriteLine("Stoinostta na poslednoto chislo e:" + poreden_chlen);
