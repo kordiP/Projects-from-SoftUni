6a89aef9b510acfb2a6466d178bbbfbf6489b3bb
kordiP <nasikordov@gmail.com>
2022-07-26 23:44:23 +0300

    Added suma-chetni-nechetni --> Has 2 sums - 1 for evens and 1 for odds.

---
+++ suma-chetni-nechetni/suma chetni nechetni/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+int n = int.Parse(Console.ReadLine());
+int s1=0, s2=0;
+int k = n;
+for (int i = 0; i <= k; i++)
+{
+    if (n % 2 == 0) 
+    {
+        s1 = s1 + n;
+    }
+    else
+    {
+        s2 = s2 + n;
+    }
+    n=n-1;
+}
+Console.WriteLine("s1= "+s1);
+Console.WriteLine("s2= "+s2);
