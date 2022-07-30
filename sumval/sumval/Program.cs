1617a3b7825d442809da91ae7fb8b05aa2133cc6
kordiP <nasikordov@gmail.com>
2022-07-31 00:40:45 +0300

    Added sumval --> Sums a 'n' number of values.

---
+++ sumval/sumval/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sumval
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n; 
+            string s; 
+            s = Console.ReadLine();
+            n = int.Parse(s);
+            int sum = 0; 
+            for (int i = 1; i<= n; i++)
+{
+                s = Console.ReadLine();
+                int number = int.Parse(s);
+                sum = sum + number;
+            }
+            Console.WriteLine("Sum:{0}", sum);
+        }
+    }
+}
