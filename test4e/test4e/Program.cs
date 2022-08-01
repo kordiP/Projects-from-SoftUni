352e3056ade8b2c1dfa2a69b7a0793c5781324ba
kordiP <nasikordov@gmail.com>
2022-08-01 23:45:15 +0300

    Added test4e --> Prints a given ammount of money to dollars and cents.

---
+++ test4e/test4e/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace test4e
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lev = 10, st=25;
+            Console.WriteLine("sumata e {0,5} leva i {1,1} stotinki", lev,st);
+        }
+    }
+}
