128b86b4cf8186064c1d49e6321c9efc4d6ea90b
kordiP <nasikordov@gmail.com>
2022-08-02 23:47:40 +0300

    Added Test-again-I-know --> Outputs date and temperature.

---
+++ Test-again-I-know/Test again I know/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test_again_I_know
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string day = textBox1.Text;
+            string temperature = textBox2.Text;
+            label1.Text = "Temperature today "+day+" is "+temperature+" degrees.";
+        }
+    }
+}
