5705f96450361826970b780d511157d61f79c3f2
kordiP <nasikordov@gmail.com>
2022-08-10 23:31:07 +0300

    Added triagalnyk --> Based on the given sides, outputs whether a triangle exists or not.

---
+++ triagalnyk/triagalnyk/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace triagalnyk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a =int.Parse(Console.ReadLine());
+            int b =int.Parse(Console.ReadLine());
+            int c =int.Parse(Console.ReadLine());
+            if (a > 0 && b > 0 && c > 0 && a + b > c && a + c > b && a < c + b) { Console.WriteLine("ima"); }
+            else { Console.WriteLine("nqma"); }
+        }
+    }
+}
