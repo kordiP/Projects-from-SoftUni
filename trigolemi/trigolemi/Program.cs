4e56d4543f5ba48acbb02c13cf68d9a69939b4ef
kordiP <nasikordov@gmail.com>
2022-08-10 23:31:51 +0300

    Added trigolemi --> Outputs the biggest of 3 numbers.

---
+++ trigolemi/trigolemi/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace trigolemi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           var x = Console.ReadLine();
+            var y = Console.ReadLine();
+            var z = Console.ReadLine();
+            if (x > y && x > z) { Console.WriteLine("x e nai-golqmo"); }
+            if (y > x&&y>z) { Console.WriteLine("y e nai-golqmo"); }
+            if (z > x&&z>y) { Console.WriteLine("z e nai-golqmo"); }
+            
+            
+            
+        
+        }
+    }
+}
