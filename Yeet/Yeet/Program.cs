2288fc44598db63d012ebf6f189c5195750dfcfa
kordiP <nasikordov@gmail.com>
2022-08-13 00:48:32 +0300

    Added Yeet --> Several projects that print stuff.

---
+++ Yeet/Yeet/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Yeet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("2 numbers");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("a=" + a);
+            Console.WriteLine("b=" + b);
+            Console.WriteLine("a/b=" + a / b);
+            Console.WriteLine("a*b=" + a * b);
+            Console.WriteLine("a+b=" + a + b);
+            Console.WriteLine("Hello ... I don't know your name. What is it?");
+            string s = Console.ReadLine();
+            s = s + " " + s + "ov";
+            Console.WriteLine("Enlo " + s + "!");
+            Console.WriteLine("*******     *******    *      *     **           *      *    *     *     **           *******     *******");
+            Console.WriteLine("*           *          **     *     *  *         **     *    *     *     *  *         *           *");
+            Console.WriteLine("*           *          * *    *     *    *       * *    *    *     *     *    *       *           *");
+            Console.WriteLine("*******     *******    *  *   *     *      *     *  *   *    *     *     *      *     *******     *******");
+            Console.WriteLine("      *     *          *   *  *     *    *       *   *  *    *     *     *    *       *                 *");
+            Console.WriteLine("      *     *          *     **     *  *         *     **    *     *     *  *         *                 *");
+            Console.WriteLine("*******     *******    *      *     *            *      *    *******     *            *******     *******");
+            Console.WriteLine("LOl");
+            int n = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine()):     
+            Console.Write("m+n=");
+            Console.WriteLine(m+n);
+            Console.Write("m-n=");
+            Console.WriteLine(m - n);
+            Console.Write("m*n=");
+            Console.WriteLine(m * n);
+            Console.Write("m/n=");
+            Console.WriteLine(m / n);
+
+        }
+    }
+}
---
+++ Yeet/oiii/Proba.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace oiii
+{
+    class Proba
+    {
+        Console.WriteLine("Name?");
+            string s = "Kordi";
+        Console.WriteLine("Can u not" +s+"?");
+    }
+}
