ba95291c248dd8796a2883a4b5acdba236993270
kordiP <nasikordov@gmail.com>
2022-08-15 02:43:17 +0300

    Added zad --> Outputs the middle of a given word.

---
+++ zad/zad/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace zad
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string s =textBox1.Text;
+            int l = s.Length;
+            string res = s.Substring(l - 2, 2);
+            textBox2.Text = res;
+        }
+    }
+}
