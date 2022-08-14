6079bb29c42e4697fa797a82c460a2b4afa06432
kordiP <nasikordov@gmail.com>
2022-08-15 02:44:28 +0300

    Added zadacha9b --> Prints a 5x5 square with a given symbol.

---
+++ zadacha9b/zadacha9b/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadacha9b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int j = 1; j <= 5; j++)
+                {
+                    Console.Write("@ ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ zadacha9b/zadacha_2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadacha_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Въведи страна на квадрата:");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Кой символ да се отпечати?");
+            char m = char.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= n; j++)
+                {
+                    Console.Write(m);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
