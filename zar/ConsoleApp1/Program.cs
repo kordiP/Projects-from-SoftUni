eefd0443aaee4f38c2506aa81a2e8b4060920595
kordiP <nasikordov@gmail.com>
2022-08-20 23:40:37 +0300

    Added zar --> Rolls a dice

---
+++ zar/ConsoleApp1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Natisni ENTER za da hvurlish zar!");
+            Console.ReadLine();
+            Random a = new Random();
+            int zar = a.Next(1, 6);
+            if (zar==1)
+            {
+                Console.WriteLine("*");
+            }
+            if (zar == 2)
+            {
+                Console.WriteLine("*");
+                Console.WriteLine(" *");
+
+            }
+            if (zar == 3)
+            {
+                Console.WriteLine("*");
+                Console.WriteLine(" *");
+                Console.WriteLine("  *");
+            }
+            if (zar == 4)
+            {
+                Console.WriteLine("*  *");
+                Console.WriteLine("*  *");
+            }
+            if (zar == 5)
+            {
+                Console.WriteLine("*  *");
+                Console.WriteLine("  *  ");
+                Console.WriteLine("*  *");
+            }
+            if (zar == 6)
+            {
+                Console.WriteLine("*  *");
+                Console.WriteLine("*  *");
+                Console.WriteLine("*  *");
+            }
+            Console.WriteLine("_");
+
+        }
+    }
+}
