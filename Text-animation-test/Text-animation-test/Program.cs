9b407ed1cc54aaabfe0e8317e952643a04699d31
kordiP <nasikordov@gmail.com>
2022-11-07 10:57:24 +0200

    Added Text-animation-test --> Made a text animated by making the letters appear with a delay.

---
+++ Text-animation-test/Text-animation-test/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading;
+
+namespace Text_animation_test
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string text = "Hi! This is a test to see if I can make something like an animation for text in C#!\nIt worked first try!\nHonestly, amazing!";
+            for (int i = 0; i < text.Length; i++)
+            {
+                Console.Write(text[i]);
+                Thread.Sleep(69);
+            }
+        }
+    }
+}
