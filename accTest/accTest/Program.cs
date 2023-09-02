b6c6f36736446888703510962512823cff438992
kordiP <nasikordov@gmail.com>
2023-09-02 22:30:53 +0300

    finding the fastest way to visualize a sum with given coins

---
+++ accTest/accTest/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace accTest
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            List<string> list1 = new() { "a", "b", "c", "d", "e", "f" };
+            List<string> list2 = new() { "g", "h", "i", "j", "k", "l" };
+
+            list1.AddRange(list2);
+
+            foreach (string item in list1) { Console.Write(item + " "); }
+        }
+
+    }
+}
---
+++ m10_Algorythms/m10_Algorythms/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace m10_Algorythms
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int wantedSum = 48;
+            int[] coinsValues = new int[] { 25, 10, 5, 1 };
+            int currentSum = 0;
+            Queue<int> coinsNeeded = new Queue<int>(); 
+
+            while (currentSum!=wantedSum) 
+            {
+                for (int i = 0; i < coinsValues.Count(); i++)
+                {
+                    if (!(currentSum + coinsValues[i] > wantedSum))
+                    {
+                        coinsNeeded.Enqueue(coinsValues[i]);
+                        break;
+                    }
+                }
+                currentSum = coinsNeeded.Sum();
+            }
+            foreach (var item in coinsNeeded)
+            {
+                Console.Write(item + " ");
+            }
+        }
+    }
+}
