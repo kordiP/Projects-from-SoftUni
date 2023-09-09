01dbfb8c06dbb8ec4fec778ed424e6b8c84545d3
kordiP <nasikordov@gmail.com>
2023-09-09 12:45:16 +0300

    fibonachi's numbers algortythm

---
+++ m10_Algorythms/Fibounachi/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Fibounachi
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int number = int.Parse(Console.ReadLine());
+            int fibonachiNumber = FibonachiRecoursiveAlgorithm(number);
+            Console.WriteLine(fibonachiNumber);
+        }
+        static int FibonachiRecoursiveAlgorithm(int number)
+        {
+            if (number == 1 || number == 2)
+            {
+                return 1;
+            }
+            return FibonachiRecoursiveAlgorithm(number - 1) + FibonachiRecoursiveAlgorithm(number - 2);
+        }
+    }
+}
