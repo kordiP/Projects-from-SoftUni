35fa17238723230484a85d743b2bd653dd06f9b5
kordiP <nasikordov@gmail.com>
2023-09-26 23:52:12 +0300

    finds all combinations where sum of numbers equal 0

---
+++ m10_Algorythms/permutations/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Channels;
+
+namespace permutations
+{
+    internal class Program
+    {
+        static void VariationFinder(string currentPattern, int currentSum, int num, int[] numbers)
+        {
+            if (num != numbers.Count())
+            {
+                VariationFinder(currentPattern + "+" + numbers[num], currentSum + numbers[num], num + 1, numbers);
+                VariationFinder(currentPattern + "-" + numbers[num], currentSum - numbers[num], num + 1, numbers);
+            }
+            else
+            {
+                if (currentSum == 0) Console.WriteLine(currentPattern);
+            }
+        }
+        static void Main()
+        {
+            int numCount = int.Parse(Console.ReadLine());
+            int[] numbers = Array.ConvertAll(Console.ReadLine().Split(' '), Convert.ToInt32);
+            VariationFinder("", 0, 0, numbers);
+        }
+    }
+
+}
